66536c2a0864b5574b19b248bc365fc31cb24cf3
avbprogramming <avb.programming@gmail.com>
2022-01-02 23:46:23 +0300

    add 02.01.22

--- src/JavaSyntaxMassive.java
+++ src/JavaSyntaxMassive.java
@@ -19,8 +19,35 @@ import java.util.concurrent.ThreadLocalRandom;
 
 
 public class JavaSyntaxMassive {
-    public static int[][] multiArray;
 
+    public static String[] strings = new String[]{"I", "love", "Java"};
+    public static int[] ints = new int[]{1, 2, 3, 4, 5, 6, 7, 8};
+
+    public static void main(String[] args) {
+        //напишите тут ваш код
+    }
+}
+
+
+
+    /*
+    //Java Rush Java Rush Java Rush Java Rush Java Rush Java Rush Java Rush Java Rush Java Rush Java Rush Java Rush Java Rush Java Rush
+//Создаем мультимассив
+    public static int[][][] multiArray = new int[][][]{{{4, 8, 15}, {16}}, {{23, 42}, {}}, {{1}, {2}, {3}, {4, 5}}};
+    public static void main(String[] args) {
+        for (int i = 0; i < multiArray.length; i++) {
+            for (int j = 0; j < multiArray[i].length; j++) {
+                for (int k = 0; k < multiArray[i][j].length; k++) {
+                    System.out.print(multiArray[i][j][k]);
+                }
+            }
+        }
+    }
+}
+//Java Rush Java Rush Java Rush Java Rush Java Rush Java Rush Java Rush Java Rush Java Rush Java Rush Java Rush Java Rush Java Rush
+    /*
+Создаем двумерный массив
+    public static int[][] multiArray;
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
